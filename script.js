d6429559550e673d9ac6a6cfddd867b235206105
snehajadhav113 <sj6275179@gmail.com>
2025-03-24 15:33:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Example: Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
